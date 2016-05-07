aad71ae436c73f4b8fc3b3ee935fdbf064fd241b
_InDev_ <aguy867@gmail.com>
2016-05-07 17:41:50 -0400

    EstiChat 1.0.0

---
+++ src/net/estinet/gFeatures/Feature/EstiChat/EstiChat.java
@@ -0,0 +1,33 @@
+package net.estinet.gFeatures.Feature.EstiChat;
+
+import net.estinet.gFeatures.Events;
+import net.estinet.gFeatures.Retrieval;
+import net.estinet.gFeatures.gFeature;
+import net.md_5.bungee.api.event.PlayerHandshakeEvent;
+import net.md_5.bungee.api.plugin.Event;
+
+public class EstiChat extends gFeature implements Events{
+	
+	EventHub eh = new EventHub();
+	
+	public EstiChat(String featurename, String d) {
+		super(featurename, d);
+	}
+	@Override
+	public void enable(){
+		Enable.onEnable();
+	}
+	@Override
+	public void disable(){
+		Disable.onDisable();
+	}
+	@Override
+	public void eventTrigger(Event event) {
+		if(event.getClass().getName().equalsIgnoreCase("playerhandshakeevent")){
+			eh.onPlayerJoin((PlayerHandshakeEvent)event);
+		}
+	}
+	@Override
+	@Retrieval
+	public void onPlayerHandshake(){}
+}
