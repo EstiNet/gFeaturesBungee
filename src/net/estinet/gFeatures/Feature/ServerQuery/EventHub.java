45149275022784603b0a8b697b0f2cfc6744a19b
_InDev_ <aguy867@gmail.com>
2016-07-25 14:27:11 -0400

    Adding ServerQuery

---
+++ src/net/estinet/gFeatures/Feature/ServerQuery/EventHub.java
@@ -0,0 +1,11 @@
+package net.estinet.gFeatures.Feature.ServerQuery;
+
+import net.md_5.bungee.api.event.PostLoginEvent;
+
+public class EventHub {
+
+	public void onPlayerJoin(PostLoginEvent event) {
+		
+	}
+
+}
---
+++ src/net/estinet/gFeatures/Feature/ServerQuery/ServerQuery.java
@@ -0,0 +1,35 @@
+package net.estinet.gFeatures.Feature.ServerQuery;
+
+import net.estinet.gFeatures.Events;
+import net.estinet.gFeatures.Retrieval;
+import net.estinet.gFeatures.gFeature;
+import net.md_5.bungee.api.event.PlayerHandshakeEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Event;
+
+public class ServerQuery extends gFeature implements Events{
+	
+	EventHub eh = new EventHub();
+	
+	public ServerQuery(String featurename, String d) {
+		super(featurename, d);
+	}
+	@Override
+	public void enable(){
+		Enable.onEnable();
+	}
+	@Override
+	public void disable(){
+		Disable.onDisable();
+	}
+	@Override
+	public void eventTrigger(Event event) {
+		if(event.getClass().getName().substring(26, event.getClass().getName().length()).equalsIgnoreCase("postloginevent")){
+			eh.onPlayerJoin((PostLoginEvent)event);
+		}
+	}
+	@Override
+	@Retrieval
+	public void onPostLogin(){}
+	
+}
