c4f619b572b18cbe47aa8dba487b106fc804661e
espidev <aguy867@gmail.com>
2016-12-03 21:44:58 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/FusionPlay/Commands/FusionPlayCommand.java
+++ src/net/estinet/gFeatures/Feature/FusionPlay/Commands/FusionPlayCommand.java
@@ -2,6 +2,9 @@ package net.estinet.gFeatures.Feature.FusionPlay.Commands;
 
 import net.estinet.gFeatures.EstiCommand;
 import net.estinet.gFeatures.gFeature;
+import net.estinet.gFeatures.Feature.FusionPlay.FusionCon;
+import net.estinet.gFeatures.Feature.FusionPlay.FusionPlay;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 
 public class FusionPlayCommand extends EstiCommand{
@@ -11,10 +14,20 @@ public class FusionPlayCommand extends EstiCommand{
 	}
 
 	@Override
-	public void execute(CommandSender arg0, String[] args) {
+	public void execute(CommandSender sender, String[] args) {
 		if(args.length == 1){
+			args[0] = args[0].toLowerCase();
 			switch(args[0]){
-			
+			case "help":
+				sender.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "----------" + ChatColor.RESET + "" + ChatColor.AQUA + "FusionPlay Help" + ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "----------");
+				sender.sendMessage(ChatColor.DARK_GRAY + "/fusionplay list : Lists all connection data.");
+				break;
+			case "list":
+				sender.sendMessage(ChatColor.AQUA + "FusionPlay List:");
+				for(FusionCon fc : FusionPlay.getConnections()){
+					sender.sendMessage(ChatColor.DARK_AQUA + "");
+				}
+				break;
 			}
 		}
 	}
