ec477294ebb75cb81a87691ecff0abcc0306370e
espidev <aguy867@gmail.com>
2016-12-18 19:45:54 -0500

    i really hate you bungeecord api

--- src/net/estinet/gFeatures/Feature/Alerts/AlertClioteHook.java
+++ src/net/estinet/gFeatures/Feature/Alerts/AlertClioteHook.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import net.estinet.gFeatures.gFeature;
 import net.estinet.gFeatures.ClioteSky.API.ClioteHook;
-import net.md_5.bungee.api.BungeeTitle;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.Title;
 
 public class AlertClioteHook extends ClioteHook{
 
@@ -24,8 +24,11 @@ public class AlertClioteHook extends ClioteHook{
 			}
 			TextComponent tc = new TextComponent(message);
 			
-			BungeeTitle bt = new BungeeTitle();
+			Title bt = ProxyServer.getInstance().createTitle().reset();
 			bt.title(new TextComponent(ChatColor.BOLD + "[" + ChatColor.RED + "Alert" + ChatColor.RESET + "" + ChatColor.BOLD + "]"));
+			bt.fadeIn(2);
+			bt.stay(4);
+			bt.fadeOut(2);
 			bt.subTitle(tc);
 			for(ProxiedPlayer p : ProxyServer.getInstance().getPlayers()){
 				p.sendTitle(bt);
