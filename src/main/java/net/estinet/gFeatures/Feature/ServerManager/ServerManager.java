7cb95e3565d682a3580b5b35ccb0b5558cc52820
EspiDev <espidev@gmail.com>
2018-06-12 21:50:45 -0400

    Adding serverman

--- src/main/java/net/estinet/gFeatures/Feature/ServerManager/ServerManager.java
+++ src/main/java/net/estinet/gFeatures/Feature/ServerManager/ServerManager.java
@@ -1,7 +1,10 @@
 package net.estinet.gFeatures.Feature.ServerManager;
 
 import net.estinet.gFeatures.Events;
+import net.estinet.gFeatures.Retrieval;
 import net.estinet.gFeatures.gFeature;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Event;
 
 public class ServerManager extends gFeature implements Events {
@@ -12,16 +15,30 @@ public class ServerManager extends gFeature implements Events {
 
     @Override
     public void enable() {
-
+        ProxyServer.getInstance().getLogger().info("[ServerManager] Enabled!");
     }
 
     @Override
     public void disable() {
-
+        ProxyServer.getInstance().getLogger().info("[ServerManager] Disabled!");
     }
 
     @Override
     public void eventTrigger(Event event) {
+        if (event.getClass().getName().substring(26, event.getClass().getName().length()).equalsIgnoreCase("serverconnectevent")) {
+            ServerConnectEvent e = (ServerConnectEvent) event;
+            e.getTarget().ping((result, error) -> {
+                if (error != null) {
+                    //Means that server is not responding : OFFLINE
+                    
+                }
+
+            });
+        }
     }
 
+    @Retrieval
+    @Override
+    public void onServerConnect() {
+    }
 }
